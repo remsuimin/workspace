6284f35e9b7370d4a20f116a2866d02bf7973730
masayoshi horita <m.h0329_medium@outlook.jp>
2021-08-27 14:18:21 +0900

    osbook_day07a

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -11,7 +11,6 @@
 #include <numeric>
 #include <vector>
 
-// #@@range_begin(includes)
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "mouse.hpp"
@@ -24,7 +23,8 @@
 #include "usb/classdriver/mouse.hpp"
 #include "usb/xhci/xhci.hpp"
 #include "usb/xhci/trb.hpp"
-// #@@range_end(includes)
+#include "interrupt.hpp"
+#include "asmfunc.h"
 
 const PixelColor kDesktopBGColor{45, 118, 237};
 const PixelColor kDesktopFGColor{255, 255, 255};
@@ -32,12 +32,9 @@ const PixelColor kDesktopFGColor{255, 255, 255};
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
-// #@@range_begin(console_buf)
 char console_buf[sizeof(Console)];
 Console* console;
-// #@@range_end(console_buf)
 
-// #@@range_begin(printk)
 int printk(const char* format, ...) {
   va_list ap;
   int result;
@@ -50,18 +47,14 @@ int printk(const char* format, ...) {
   console->PutString(s);
   return result;
 }
-// #@@range_end(printk)
 
-// #@@range_begin(mouse_observer)
 char mouse_cursor_buf[sizeof(MouseCursor)];
 MouseCursor* mouse_cursor;
 
 void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
   mouse_cursor->MoveRelative({displacement_x, displacement_y});
 }
-// #@@range_end(mouse_observer)
 
-// #@@range_begin(switch_echi2xhci)
 void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
   bool intel_ehc_exist = false;
   for (int i = 0; i < pci::num_device; ++i) {
@@ -82,9 +75,22 @@ void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
   Log(kDebug, "SwitchEhci2Xhci: SS = %02, xHCI = %02x\n",
       superspeed_ports, ehci2xhci_ports);
 }
-// #@@range_end(switch_echi2xhci)
 
-// #@@range_begin(call_pixel_writer)
+
+usb::xhci::Controller* xhc;
+
+__attribute__((interrupt))
+void IntHandlerXHCI(InterruptFrame* frame) {
+  while (xhc->PrimaryEventRing()->HasFront()) {
+    if (auto err = ProcessEvent(*xhc)) {
+      Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
+          err.Name(), err.File(), err.Line());
+    }
+  }
+  NotifyEndOfInterrupt();
+}
+
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
   	case kPixelRGBResv8BitPerColor:
@@ -100,7 +106,6 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   const int kFrameWidth = frame_buffer_config.horizontal_resolution;
   const int kFrameHeight = frame_buffer_config.vertical_resolution;
 
-  // #@@range_begin(draw_desktop)
   FillRectangle(*pixel_writer,
                 {0, 0},
                 {kFrameWidth, kFrameHeight - 50},
@@ -117,22 +122,17 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
                 {10, kFrameHeight - 40},
                 {30, 30},
                 {160, 160, 160});
-  
+
   console = new(console_buf) Console{
     *pixel_writer, kDesktopFGColor, kDesktopBGColor
   };
   printk("Welcome to Mikan-chang OS!\n");
   SetLogLevel(kWarn);
-  // #@@range_end(draw_desktop)
 
-
-  // #@@range_begin(new_mouse_cursor)
   mouse_cursor = new(mouse_cursor_buf) MouseCursor{
     pixel_writer, kDesktopBGColor, {300, 200}
   };
-  // #@@range_end(new_mouse_cursor)
 
-  // #@@range_begin(show_devices)
   auto err = pci::ScanAllBus();
   Log(kDebug, "ScanAllBus: %s\n", err.Name());
 
@@ -144,9 +144,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
         dev.bus, dev.device, dev.function,
         vendor_id, class_code, dev.header_type);
   }
-  // #@@range_end(show_devices)
 
-  // #@@range_begin(find_xhc)
   // Intel 製を優先して xHC を探す
   pci::Device* xhc_dev = nullptr;
   for (int i = 0; i < pci::num_device; ++i) {
@@ -163,16 +161,28 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     Log(kInfo, "xHC has been found: %d.%d.%d\n",
         xhc_dev->bus, xhc_dev->device, xhc_dev->function);
   }
-  // #@@range_end(find_xhc)
 
-  // #@@range_begin(read_bar)
+
+  const uint16_t cs = GetCS();
+  SetIDTEntry(idt[InterruptVector::kXHCI], MakeIDTAttr(DescriptorType::kInterruptGate, 0),
+              reinterpret_cast<uint64_t>(IntHandlerXHCI), cs);
+  LoadIDT(sizeof(idt) - 1, reinterpret_cast<uintptr_t>(&idt[0]));
+
+
+
+  const uint8_t bsp_local_apic_id =
+    *reinterpret_cast<const uint32_t*>(0xfee00020) >> 24;
+  pci::ConfigureMSIFixedDestination(
+      *xhc_dev, bsp_local_apic_id,
+      pci::MSITriggerMode::kLevel, pci::MSIDeliveryMode::kFixed,
+      InterruptVector::kXHCI, 0);
+
+
   const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
   Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
   const uint64_t xhc_mmio_base = xhc_bar.value & ~static_cast<uint64_t>(0xf);
   Log(kDebug, "xHC mmio_base = %08lx\n", xhc_mmio_base);
-  // #@@range_end(read_bar)
 
-  // #@@range_begin(init_xhc)
   usb::xhci::Controller xhc{xhc_mmio_base};
 
   if (0x8086 == pci::ReadVendorId(*xhc_dev)) {
@@ -185,9 +195,10 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
 
   Log(kInfo, "xHC starting\n");
   xhc.Run();
-  // #@@range_end(init_xhc)
 
-  // #@@range_begin(configure_port)
+  ::xhc = &xhc;
+  __asm__("sti");
+
   usb::HIDMouseDriver::default_observer = MouseObserver;
 
   for (int i = 1; i <= xhc.MaxPorts(); ++i) {
@@ -202,20 +213,9 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       }
     }
   }
-  // #@@range_end(configure_port)
-
-  // #@@range_begin(receive_event)
-  while (1) {
-    if (auto err = ProcessEvent(xhc)) {
-      Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
-          err.Name(), err.File(), err.Line());
-    }
-  }
-  // #@@range_end(receive_event)
 
   while (1) __asm__("hlt");
 }
-// #@@range_end(call_pixel_writer)
 
 extern "C" void __cxa_pure_virtual() {
   while (1) __asm__("hlt");
